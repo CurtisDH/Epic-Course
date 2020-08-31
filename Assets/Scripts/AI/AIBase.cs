5c3f3e0c0efdb353daca584dd1430416f9e58006
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-01 01:31:07 +1000

    Create a Base AI Class - Added a target for the AI

---
+++ Assets/Scripts/AI/AIBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+public class AIBase : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public float health;
+    public int warFund;
+    private void Start()
+    {
+        if (GetComponent<NavMeshAgent>() != null) 
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+        else // If there is no NavmeshAgent then create one.
+        {
+            gameObject.AddComponent<NavMeshAgent>();
+            agent = GetComponent<NavMeshAgent>();
+        }
+        MoveTo(GameObject.Find("Base").transform.position);
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+
+
+
+}
