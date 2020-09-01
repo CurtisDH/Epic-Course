ae7d0d5dde6a7b73869e30040fdcb3c3ffe43214
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-01 13:03:49 +1000

    Created infinte spawning loop with a wave sytem

--- Assets/Scripts/AI/Spawn Routine/SpawnManager.cs
+++ Assets/Scripts/AI/Spawn Routine/SpawnManager.cs
@@ -11,6 +11,12 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     Vector3 StartPos, EndPos;
 
+    [SerializeField]
+    List<GameObject> Wave;
+    private readonly int AmountToSpawn = 10;
+    private int CurrentWave;
+
+    float timeBetweenWave = 2f;
 
     private void Start()
     {
@@ -18,12 +24,12 @@ public class SpawnManager : MonoBehaviour
     }
     IEnumerator SpawnRoutine() 
     {
-        while(true)
+        for(int i = 0; i < Wave.Count; i++)
         {
-            yield return new WaitForSeconds(1);
-            var enemy = Instantiate(Enemies[Random.Range(0,Enemies.Length)]); // might change to create a spawn list of enemies which are randomly defined for that wave.
+            yield return new WaitForSeconds(timeBetweenWave);
+            var enemy = Instantiate(Enemies[Random.Range(0, Enemies.Length)]); // might change to create a spawn list of enemies which are randomly defined for that wave.
             enemy.transform.position = StartPos;
-            if(enemy.GetComponent<AIBase>())
+            if (enemy.GetComponent<AIBase>())
             {
                 enemy.GetComponent<AIBase>().InitaliseAI();
                 enemy.GetComponent<NavMeshAgent>().Warp(StartPos); // had issues with the agent saying "NO AGENT ON NAVMESH" using Warp fixes this.
@@ -32,9 +38,34 @@ public class SpawnManager : MonoBehaviour
             }
             else
             {
-                 Debug.LogError("Couldn't find Component AIBase");
+                Debug.LogError("Couldn't find Component AIBase");
+            }
+            
+        }
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            if (GameObject.Find("EnemyContainer").transform.childCount == 0)
+            {
+                //Display wave complete?
+                Debug.Log("EnemyContainer empty.. Spawning new Wave");
+                CreateWave();
+                break;
             }
         }
+
+    }
+
+    void CreateWave() //Randomly populate a list with enemy types. List size depends on current wave * base amount to spawn.
+    {
+        CurrentWave++;
+        Wave.Clear();
+        for(int i = 0; i < (AmountToSpawn * CurrentWave); i++)
+        {
+            Wave.Add(Enemies[Random.Range(0, Enemies.Length)]);
+        }
+        StartCoroutine(SpawnRoutine());
+        
 
     }
 
