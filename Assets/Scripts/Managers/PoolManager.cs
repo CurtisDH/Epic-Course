36d058d1c0ccc63534378a09d644784c71e6c963
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-07 11:07:30 +1000

    Reorganised Manager scripts with NameSpaces

---
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CurtisDH.Scripts.Managers
+{
+    public class PoolManager : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> _pooledObjects; //change to private
+        public List<GameObject> PooledObjects
+        {
+            get
+            {
+                return _pooledObjects;
+            }
+        }
+        private static PoolManager _instance;
+        public static PoolManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogError("PoolManager is NULL creating one..");
+                    var PoolManager = new GameObject("PoolManager");
+                    PoolManager.AddComponent<PoolManager>();
+                    Debug.Log("Created");
+                }
+                return _instance;
+            }
+        }
+        private void Awake()
+        {
+            _instance = this;
+        }
+        public void ObjectsReadyToRecycle(GameObject obj) // when object setActive = false (In the die method) add it to this list.
+        {
+            PooledObjects.Add(obj);
+        }
+        public GameObject RequestEnemy()
+        {
+            return _pooledObjects?[0];
+        }
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CurtisDH.Scripts.Managers
+{
+    using CurtisDH.Utilites;
+    using UnityEngine.AI;
+
+    public class SpawnManager : MonoBehaviour
+    {
+        private static SpawnManager _instance;
+        public static SpawnManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.Log("SpawnManager Instance is NULL.. Attempting to lazy instantiate ");
+                    var SpawnManager = new GameObject("SpawnManager");
+                    SpawnManager.AddComponent<SpawnManager>();
+                    Debug.Log("Created a SpawnManager");
+                }
+                return _instance;
+            }
+        }
+        [SerializeField]
+        GameObject[] _enemies; //change to _enemies
+
+        public GameObject[] Enemies
+        {
+            get
+            {
+                return _enemies;
+            }
+        }
+        [SerializeField]
+        private Vector3 _startPos, _endPos;
+        public Vector3 StartPos
+        {
+            get
+            {
+                return _startPos;
+            }
+        }
+        public Vector3 EndPos
+        {
+            get
+            {
+                return _endPos;
+            }
+        }
+
+        [SerializeField]
+        List<GameObject> Wave;
+        private int _amountToSpawn = 10;// add _
+        private int _currentWave;
+
+        float timeBetweenWave = 2f;
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+        private void Start()
+        {
+            CreateWave();
+        }
+        IEnumerator SpawnRoutine()
+        {
+            if (PoolManager.Instance.PooledObjects.Count > 1)
+            {
+                Utilites.RandomiseList(PoolManager.Instance.PooledObjects); // Changed to make it only randomise the list once
+            }
+            for (int i = 0; i < Wave.Count; i++)
+            {
+                //Debug.Log(Wave.Count);
+                yield return new WaitForSeconds(timeBetweenWave);
+                SpawnEnemy();
+            }
+            while (true)
+            {
+                yield return new WaitForSeconds(2);
+                if (GameObject.Find("EnemyContainer").transform.childCount == 0)
+                {
+                    //Display wave complete?
+                    //Debug.Log("EnemyContainer empty.. Spawning new Wave");
+                    CreateWave();
+                    break;
+                }
+            }
+        }
+
+        void SpawnEnemy()
+        {
+            try
+            {
+                PoolManager.Instance.RequestEnemy().SetActive(true);
+            }
+            catch
+            {
+                Instantiate(Enemies[Random.Range(0, Enemies.Length)]);
+            }
+        }
+
+        void CreateWave() //Randomly populate a list with enemy types. List size depends on current wave * base amount to spawn.
+        {
+            _currentWave++;
+            Wave.Clear();
+            for (int i = 0; i < (_amountToSpawn * _currentWave); i++)
+            {
+                Wave.Add(Enemies[Random.Range(0, Enemies.Length)]);
+            }
+            StartCoroutine(SpawnRoutine());
+        }
+    }
+
+}
