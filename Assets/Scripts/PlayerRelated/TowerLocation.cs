61e9f7e2cf6110af3be94938e22f61ba0690e3b8
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-11 13:31:32 +1000

    Added TowerLocation Script holding turret information & occupied status

---
+++ Assets/Scripts/PlayerRelated/TowerLocation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLocation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject TurretOccupying;
+    [SerializeField]
+    bool _isOccupied;
+    public bool IsOccupied
+    {
+        get
+        {
+            return _isOccupied;
+        }
+    }
+
+    public void PlaceTower(GameObject obj)
+    {
+        TurretOccupying = obj;
+        TurretOccupying.transform.position = this.transform.position;
+        _isOccupied = true;
+    }
+    public void RemoveTower()
+    {
+        TurretOccupying = null;
+        _isOccupied = false;
+    }
+}
