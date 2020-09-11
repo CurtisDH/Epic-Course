ba57bea21ea41343484abddda86a8b07ac749529
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-11 12:17:53 +1000

    Created TowePlacement script - Raycasting mouse pos

--- Assets/Scripts/PlayerRelated/TowerConstruction.cs
+++ Assets/Scripts/PlayerRelated/TowerConstruction.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using CurtisDH.Utilites;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class TowerConstruction : MonoBehaviour
 {
@@ -8,6 +10,13 @@ public class TowerConstruction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction, Color.green);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.name);
+
+        }
     }
 }
