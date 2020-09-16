4e46fab836f7500dd7844acadea15363a03b0ece
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-09-16 14:15:11 +1000

    Added a event system to detect if enemy has entered tower radius.

---
+++ Assets/Scripts/PlayerRelated/Towers/TowerEnemyDetection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerEnemyDetection : MonoBehaviour
+{
+   public static Action<GameObject,GameObject> onEnemyDetectionRadius;
+    private void OnTriggerEnter(Collider other)
+    {
+        onEnemyDetectionRadius?.Invoke(other.gameObject, this.transform.root.gameObject);
+    }
+}
